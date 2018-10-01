e5b4b3e66eac1e1e769163254e8dd8be81964f8c
masakatsu iida <iida.masa@cntsv.jp>
2018-10-01 23:46:08 +0900

    add coding_policy.md

---
+++ formater_002.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import csv
+import json
+
+class Getqb(object):
+
+    def __init__(self,file_name):
+        self.file_name = file_name
+
+    def get_header(self):
+        df = pd.read_json(self.file_name)
+        print(df)
+        return df[0]
+
+    def write_to_csv(self):
+        df = pd.read_json(self.file_name)
+        # CSV ファイルとして出力
+        # indexを出力対象から外したい場合はindex = Falseを指定。
+        df.to_csv("output/output.csv", index=False)
+        return self.file_name
+
