6139d743b4bc847541c992e39da31dd1fdefaf92
Masa Iida <iida.m@cntsv.jp>
2018-10-07 11:15:00 +0900

    add dfmarge.py

---
+++ dfmarge.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+init_val = 99
+
+df = pd.DataFrame(
+    [
+     {"lcd": "E00", "product": "item1", "price": 100},
+     {"lcd": "E01", "product": "item1", "price": ''},
+     {"lcd": "E02", "product": "item1", "price": ''},
+     {"lcd": "F00", "product": "item1", "price": 300},
+     {"lcd": "F01", "product": "item1", "price": ''},
+     {"lcd": "F02", "product": "item1", "price": ''}
+     ],
+    columns=["lcd", "product", "price"]
+)
+
+mst_e = pd.DataFrame(
+    [{"lcd": "E01", "price": 123}],
+    columns=["lcd", "price"]
+)
+
+mst_o = pd.DataFrame(
+    [{"lcd": "F01", "price": 456}],
+    columns=["lcd", "price"]
+)
+
+# df2 = pd.DataFrame(
+#     {
+#         "lcd": ["E00", "E00","E00", "E00"],
+#         "price": [100, 100, 100, 200]
+#     }
+# )
+
+# print(mst_e)
+# print(mst_o)
+print(df)
+
+mst_e = mst_e.rename(columns={'price': 'e_price'})
+mst_o = mst_o.rename(columns={'price': 'o_price'})
+print(mst_e)
+print(mst_o)
+
+# print(pd.concat([mst_e, mst_o]))
+# print(pd.concat([df, mst_o], axis=1))
+
+# df = pd.concat([df, mst_o], axis=1, join_axes=[df.index])
+# df = pd.concat([df, mst_e], axis=1, join_axes=[df.index])
+# df = pd.merge(df, mst_o, on='lcd')
+
+df = pd.merge(df, mst_o, on='lcd', how='left')
+df = pd.merge(df, mst_e, on='lcd', how='left')
+print(df)
